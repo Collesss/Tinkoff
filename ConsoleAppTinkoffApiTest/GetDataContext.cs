d53272ced395485d0a00302e252182720cc7b14b
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-11-08 02:15:32 +0300

    Init

---
+++ ConsoleAppTinkoffApiTest/GetDataContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tinkoff.Trading.OpenApi.Models;
+using Tinkoff.Trading.OpenApi.Network;
+
+namespace ConsoleAppTinkoffApiTest
+{
+    public class GetDataContext
+    {
+        private readonly Context _context;
+
+        private readonly TimeSpan _maxRequestTime;
+        private readonly int _maxRequestCount;
+
+        private DateTime _dateTimeLastSendRequest;
+        private int _requestCount;
+
+        private object locker = new object();
+
+        public GetDataContext(Context context, int maxRequestCount, TimeSpan maxRequestTime)
+        {
+            _context = context;
+            _maxRequestTime = maxRequestTime;
+            _maxRequestCount = maxRequestCount;
+        }
+
+        public IEnumerable<CandlePayload> GetData(string figi, CandleInterval candleInterval, DateTime from, DateTime to)
+        {
+            TinkoffCuterRequest tcr = new TinkoffCuterRequest(candleInterval, from, to);
+            
+            return tcr.Select(async dateFromTo =>
+            {
+                lock (locker)
+                {
+                    if (_requestCount == _maxRequestCount)
+                    {
+                        _requestCount = 0;
+
+                        TimeSpan diffWaitTime = DateTime.Now - _dateTimeLastSendRequest;
+
+                        if (_maxRequestTime > diffWaitTime)
+                            Task.Delay((_maxRequestTime - diffWaitTime).Add(TimeSpan.FromMilliseconds(50))).Wait();
+                    }
+
+                    _requestCount++;
+                    _dateTimeLastSendRequest = DateTime.Now;
+                }
+
+                return await _context.MarketCandlesAsync(figi, dateFromTo.from, dateFromTo.to, CandleInterval.Hour);
+            }).SelectMany(candles => candles.Result.Candles)
+            .OrderBy(candle => candle.Time)
+            .ToArray();
+        }
+    }
+}
---
+++ ConsoleAppTinkoffApiTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Tinkoff.Trading.OpenApi.Models;
+using Tinkoff.Trading.OpenApi.Network;
+
+namespace ConsoleAppTinkoffApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test().Wait();
+
+            Console.Write("press any key...");
+            Console.ReadKey();
+        }
+
+
+        public async static Task Test()
+        {
+            // токен аутентификации
+            var token = "t.F-e_MvGHyM5RydIcD28rwvIuOpgfChfOokIlqKWYOm9JKUeJFLQwlZMP0O6p_hneiDWOAjT90UQzJSlEvBZSog";
+            // для работы в песочнице используйте GetSandboxConnection
+            var connection = ConnectionFactory.GetConnection(token);
+            var context = connection.Context;
+
+            //TinkoffCuterRequest
+
+            int days = 2000;
+
+            Console.WriteLine(new TinkoffCuterRequest(CandleInterval.Hour, DateTime.Now - TimeSpan.FromDays(days), DateTime.Now).CountRequest);
+
+            foreach (var FromTo in new TinkoffCuterRequest(CandleInterval.Hour, DateTime.Now - TimeSpan.FromDays(days), DateTime.Now))
+            {
+                Console.WriteLine($"{FromTo.from}; {FromTo.to};");
+            }
+
+            Console.WriteLine();
+
+            GetDataContext getDataContext = new GetDataContext(context, 240, TimeSpan.FromMinutes(1));
+
+            var candles = getDataContext.GetData("BBG000T88BN2", CandleInterval.Hour, DateTime.Now - TimeSpan.FromDays(days), DateTime.Now);
+
+            /*
+            var candles = new TinkoffCuterRequest(CandleInterval.Hour, DateTime.Now - TimeSpan.FromDays(days), DateTime.Now)
+                .Select(async dateFromTo =>
+            await context.MarketCandlesAsync("BBG000T88BN2", dateFromTo.from, dateFromTo.to, CandleInterval.Hour))
+                .SelectMany(cutCandlesReq => cutCandlesReq.Result.Candles)
+                .GroupBy(candle => GetGroup(candle.Time))
+                .Select(group => Data.AgregateCandle(group))
+                .OrderBy(DataCandle => DataCandle.OpenTime);
+            
+            */
+            foreach (var candle in candles)
+            {
+                Console.WriteLine($"{candle.Time}; {candle.Open}; {candle.Close}; {candle.Low}; {candle.High}; {candle.Volume}");
+            }
+            
+        }
+
+        static string GetGroup(DateTime dateTime)
+        {
+            ValueInRange<DateTime, TimeSpan, int> valueInRange = new ValueInRange<DateTime, TimeSpan, int>(
+                new (TimeSpan, int)[]
+                {
+                    (TimeSpan.FromHours(11), 1),
+                    (TimeSpan.FromHours(15), 2),
+                    (TimeSpan.FromHours(19), 3),
+                    (TimeSpan.FromHours(23), 4)
+                },
+                (value, rangeValue) => value.Hour < rangeValue.TotalHours);
+
+            return $"{dateTime.Year}{dateTime.Month}{dateTime.Day}{valueInRange[dateTime]}";
+        }
+    }
+}
