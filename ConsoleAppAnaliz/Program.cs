0e217bf633d7b663bb100a30110a64a1625f135f
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-11-17 23:00:17 +0300

    update

---
+++ ConsoleAppAnaliz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Globalization;
+
+namespace ConsoleAppAnaliz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(new FileStream("log.txt", FileMode.Open, FileAccess.Read)))
+            {
+                var groups = reader.ReadToEnd().Split('\n').Select(ts => DateTime.Parse(ts, new CultureInfo("ru-RU"))).GroupBy(dt => $"{dt.Hour} {dt.Minute}");
+
+                foreach (var group in groups)
+                {
+                    Console.WriteLine($"group.Key|{group.Count()}");
+                }
+
+            }
+
+            Console.WriteLine("press any key...");
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleAppTinkoffApiTest/GetDataContext.cs
+++ ConsoleAppTinkoffApiTest/GetDataContext.cs
@@ -17,13 +17,14 @@ namespace ConsoleAppTinkoffApiTest
         private readonly int _maxRequestCount;
 
         private DateTime _dateTimeFirstSendRequest;
+        private DateTime _dateTimeLastSendRequest;
         private int _requestCount;
 
         private object locker = new object();
 
         public GetDataContext(Context context, int maxRequestCount, TimeSpan maxRequestTime)
         {
-            _dateTimeFirstSendRequest = DateTime.MinValue;
+            _dateTimeLastSendRequest = _dateTimeFirstSendRequest = DateTime.MinValue;
             _context = context;
             _maxRequestTime = maxRequestTime;
             _maxRequestCount = maxRequestCount;
@@ -47,17 +48,19 @@ namespace ConsoleAppTinkoffApiTest
                     {
                         _requestCount = 0;
                         
-                        Task.Delay((_maxRequestTime - (DateTime.Now - _dateTimeFirstSendRequest)).Add(TimeSpan.FromMilliseconds(250))).Wait();
+                        Task.Delay((_maxRequestTime - (DateTime.Now - _dateTimeLastSendRequest)).Add(TimeSpan.FromMilliseconds(250))).Wait();
                     }
 
                     _requestCount++;
+
+                    _dateTimeLastSendRequest = DateTime.Now;
+
+                    using (StreamWriter streamWriter = new StreamWriter(new FileStream("log.txt", FileMode.Append, FileAccess.Write)))
+                    {
+                        streamWriter.WriteLine(DateTime.Now);
+                    }
                 }
                 
-                using (StreamWriter streamWriter = new StreamWriter(new FileStream("log.txt", FileMode.Append, FileAccess.Write)))
-                {
-                    streamWriter.WriteLine(DateTime.Now);
-                }
-
                 return await _context.MarketCandlesAsync(figi, dateFromTo.from, dateFromTo.to, CandleInterval.Hour); 
             }).ToArray()))
             .SelectMany(candles => candles.Candles)
