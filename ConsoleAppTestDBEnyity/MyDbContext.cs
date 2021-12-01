dd4c2def82732b84ccf869dc84df790482008fa3
Collesss <sergey_lesikov@mail.ru>
2021-12-01 17:55:19 +0300

    update

---
+++ ConsoleAppTestDBEnyity/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppTestDBEnyity
+{
+    public class MyDbContext : DbContext
+    {
+    }
+}
---
+++ ConsoleAppTestDBEnyity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTestDBEnyity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
